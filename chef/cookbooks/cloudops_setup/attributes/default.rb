e53fea6b8586b00bc60c49ba1e98e525e869af17
HarshKini <harshkini9@gmail.com>
2025-08-16 00:03:58 +0000

    chef attrs: set dockerhub_username=hunter1994

---
+++ chef/cookbooks/cloudops_setup/attributes/default.rb
@@ -0,0 +1,3 @@
+default['cloudops']['grafana_admin_password'] = 'ChangeMe123!'   # change later
+default['cloudops']['dockerhub_username']     = 'hunter1994'     # <-- your Docker Hub ID
+default['cloudops']['ai_image_tag']           = '0.1.0'          # or 'latest' if using CI
